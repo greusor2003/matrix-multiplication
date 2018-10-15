b2d8de6b40f2731c7a02d8b99aa00c0c539646f4
greusor2003 <43874269+greusor2003@users.noreply.github.com>
2018-10-15 22:59:55 +0200

    Add files via upload

---
+++ MtrxOpenBlas.h
@@ -0,0 +1,16 @@
+//#pragma once
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <chrono>
+#include <vector>
+#include <openblas/cblas.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+typedef vector<double>::iterator vectype;
+
+void matrix_mult(vectype, size_t, size_t, vectype, size_t, size_t, vectype);
